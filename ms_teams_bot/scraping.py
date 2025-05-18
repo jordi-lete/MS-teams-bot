0a5282ddc7d9265d8f2aa5f096310baf2e10d35a
jordi-lete <jordismith20@hotmail.com>
2025-05-18 19:09:45 +0100

    Added filter to check current week and function for static testing

--- ms_teams_bot/scraping.py
+++ ms_teams_bot/scraping.py
@@ -3,8 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 # library to simulate user interaction
 from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 # Other utils
-from datetime import datetime
+import datetime
 
 # Make a request to the Playwaze website
 async def set_up_website(URL):
@@ -22,6 +23,22 @@ async def set_up_website(URL):
         html = await page.content()
         await browser.close()
         return html
+    
+def set_up_website_static(URL):
+    with sync_playwright() as p:
+        html = None
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(URL)
+        page.click("text=Fixtures", force=True)
+        page.wait_for_timeout(2000) # wait (milliseconds)
+        page.click("text=Filter by division", force=True)
+        page.wait_for_timeout(2000) # wait (milliseconds)
+        page.click("text=4pm-6pm", force=True)
+        page.wait_for_timeout(2000) # wait (milliseconds)
+        html = page.content()
+        browser.close()
+        return html
 
 # Load from a static html file
 def load_html(filepath):
@@ -29,24 +46,26 @@ def load_html(filepath):
         return f.read()
 
 async def get_fixture():
+# def get_fixture():
     timeslot = "4pm-6pm"
     team_name = "Warwick IFL 24-25 WMG FC Mixed Football 5s"
     URL = "https://bucs.playwaze.com/ims-football---warwick-24-25/cclrwgbo21zv9i/league-display/Leagues/1kjdxip63mpn"
 
     html = await set_up_website(URL)
+    # html = set_up_website_static(URL)
     # html = load_html("ms_teams_bot/web.html")
 
     soup = BeautifulSoup(html, "html.parser")
 
-    today = datetime.today()
+    today = datetime.datetime.today()
 
     fixture_dates = soup.find_all("div", class_="fixture-list-date")
     for date in fixture_dates:
         date_str = date.get("data-date")
         if not date_str:
             continue
-        date_obj = datetime.strptime(date_str, "%d%b%Y")
-        if date_obj >= today:
+        date_obj = datetime.datetime.strptime(date_str, "%d%b%Y")
+        if today <= date_obj <= today + datetime.timedelta(days=7):
             fixtures = date.find_all_next("div", class_="fixture-list")
             for fixture in fixtures:
                 # stop searching when we find the next date
@@ -59,5 +78,5 @@ async def get_fixture():
                     if opponent:
                         print(f"Next fixture: {date_str} vs {opponent[0]}")
                         return date_str, opponent[0]
-    print("No upcoming fixture found")
+    print("There is no game this week")
     return None, None
